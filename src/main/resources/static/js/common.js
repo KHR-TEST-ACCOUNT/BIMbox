5a797e56f3332e3c363ff227233639d54558c727
Kouya-Ajima <azumabass@gmail.com>
2021-10-11 08:29:57 +0900

    add:スクロール時の表示処理を追加 #01

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -244,4 +244,27 @@ $(function() {
 		});
 	});
 	
+	//フェードイン
+	
+	
+	$(function() {
+	    var topBtn = $('.fadein');    
+	    topBtn.hide();
+	    //スクロールが100に達したらボタン表示
+	    $(window).scroll(function () {
+	        if ($(this).scrollTop() > 100) {
+	            topBtn.fadeIn();
+	        } else {
+	            topBtn.fadeOut();
+	        }
+	    });
+	    //スクロールしてトップ
+	    topBtn.click(function () {
+	        $('body,html').animate({
+	            scrollTop: 0
+	        }, 500);
+	        return false;
+	    });
+	});
+
 });
