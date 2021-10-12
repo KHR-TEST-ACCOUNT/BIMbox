f6170cdfaa683077751128b14425696edaefdda7
Kouya-Ajima <azumabass@gmail.com>
2021-10-13 08:45:02 +0900

    fix:トップページの表示処理を変更 #23

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -15,6 +15,55 @@ var callbackFunction = function(data) {
 	setAddress(pref, city);
 }
 
+// ページ内スクロール
+$(function(){
+// 全てのアンカータグを対象にする
+	$('a').click(function(e){
+		var anchor = $(this);
+		href = anchor.attr('href');
+		pagename = window.location.href;
+		// 現在のurlのハッシュ以降を削除
+		pagename = pagename.replace(/#.*/,'');
+		// リンク先のurlから現在の表示中のurlを削除
+		href = href.replace( pagename , '' );
+		if( href.search(/^#/) >= 0 ){
+			// 整形したリンクがページ内リンクの場合はページ無いスクロールの対象とする
+			// 通常の遷移処理をキャンセル
+			e.preventDefault();
+			var speed = 500;
+			// 前段階で整形したhrefを使用する
+			// var href= $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top;
+			$("html, body").animate({scrollTop:position}, speed, "swing");
+			// ロケーションバーの内容を書き換え
+			location.hash = href ;
+			return false;
+		}
+	});
+});
+
+/**
+$(function(){
+	$('a[href^="#"]').click(function () {
+		let speed = 600;
+		let headerHight = $('header').html().height();
+		let href = $(this).attr("href");
+		let target = $(href == "#" || href == "" ? "html" : href);
+		let position = target.offset().top - headerHight;
+		$("body,html").animate({ scrollTop: position }, speed, "swing");
+		return false;
+	});
+});
+
+ */
+// アコーディオンメニュー
+$(function(){
+	$("#acMenu dt").on("click", function() {
+		$(this).next().slideToggle();
+		$(this).toggleClass("active");
+	});
+});
 
 $(function() {
 	
@@ -60,7 +109,7 @@ $(function() {
 			isProfEdPermission();
 		}
 		// UserRegistrationの読み込み時
-			fadeinAnimation();
+			// fadeinAnimation();
 	});
 
 	// UserProfileのデフォルトの画面表示を設定する。
