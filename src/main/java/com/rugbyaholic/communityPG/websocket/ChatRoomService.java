cce95c405146657ad4a923a3b006704e886ffe62
Kouya-Ajima <azumabass@gmail.com>
2021-10-30 00:06:39 +0900

    fix:名前の変更処理をした際の一部のDBが更新されない事象を改修 #74

--- src/main/java/com/rugbyaholic/communityPG/websocket/ChatRoomService.java
+++ src/main/java/com/rugbyaholic/communityPG/websocket/ChatRoomService.java
@@ -15,18 +15,40 @@ public class ChatRoomService {
 	@Autowired
 	private ChatRoomRepository repository;
 
+	/**
+	 * チャット中のユーザーを取得
+	 * 
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
 	@Transactional(rollbackFor = Throwable.class)
-	public List<MessageForm> getConversationalUsers(AuthenticatedUser user) throws Exception {
-		List<MessageForm> conversationalUsers = repository.findConversationalUser(user.getId());
+	public List<ChatMessage> getConversationalUsers(AuthenticatedUser user) throws Exception {
+		List<ChatMessage> conversationalUsers = repository.findConversationalUser(user.getId());
 		return conversationalUsers;
 	}
 
+	/**
+	 * メッセージ履歴を取得
+	 * 
+	 * @param user
+	 * @param toUserId
+	 * @return
+	 * @throws Exception
+	 */
 	@Transactional(rollbackFor = Throwable.class)
-	public List<MessageForm> getMessageHist(AuthenticatedUser user, Long toUserId) throws Exception {
-		List<MessageForm> messageHist = repository.findMessages(user.getId(), toUserId);
+	public List<ChatMessage> getMessageHist(AuthenticatedUser user, Long toUserId) throws Exception {
+		List<ChatMessage> messageHist = repository.findMessages(user.getId(), toUserId);
+		if(messageHist.isEmpty()) messageHist.add(new ChatMessage(toUserId));
 		return messageHist;
 	}
 
+	/**
+	 * チャットメッセージをDBに格納する
+	 * 
+	 * @param chatMessage
+	 * @throws Exception
+	 */
 	@Transactional(rollbackFor = Throwable.class)
 	public void registMessageInfo(ChatMessage chatMessage) throws Exception {
 		repository.registerMessage(chatMessage);
