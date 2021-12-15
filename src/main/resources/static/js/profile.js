e8e153bd7edf51579640d9510b506275009388d9
Kouya-Ajima <azumabass@gmail.com>
2021-12-15 22:47:01 +0900

    fix: 高さの中央揃えができな問題を改修 #168

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,16 +1,30 @@
 $(function() {
+	
 	// ロード時の window size に合わせてSideNaviを表示
 	$(window).on('load', function() {
+		rePages();
+	});
+	
+	$(window).on('resize', function() {
+		rePages();
+	});
+	
+	function rePages(){
+		var settings = $('#settings');
+		var contentHeight = $('.page-content').height();
+		var settingHeight = contentHeight - settings.height();
+		var contentTop = settingHeight / 2;
 		
-		var textarea = $('#aboutMe');
-		
+		settings.css('top', contentTop);
+	}
+	
 		/**
+		var textarea = $('#aboutMe');
 		if(textarea.val() === ''){
 			textarea.val('自分自身について（256文字以内）')
 			textarea.addClass('placeholder');
 		}
 		 */
-		
-	});
+	
 });
 
